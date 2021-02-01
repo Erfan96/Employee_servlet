fd86cd99100b1b610b855d426993a9751bc97dfe
efsam <E.salimi75@ymail.com>
2021-02-01 18:40:59 +0330

    commit Address.java in secondary

---
+++ src/main/java/entities/Address.java
@@ -0,0 +1,36 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address_emp")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @OneToOne
+    private Employee employee;
+}
