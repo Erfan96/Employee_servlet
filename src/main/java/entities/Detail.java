1292bfe766098bc750fe7f9b650d4df96ed845f9
efsam <E.salimi75@ymail.com>
2021-02-01 18:40:34 +0330

    commit Detail.java in secondary

---
+++ src/main/java/entities/Detail.java
@@ -0,0 +1,32 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "detail_emp")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Detail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "father_name")
+    private String fatherName;
+
+    @Column(name = "national_code")
+    private String nationalCode;
+
+    @Column(name = "certificate_id")
+    private String certificateId;
+
+    @OneToOne
+    private Employee employee;
+}
